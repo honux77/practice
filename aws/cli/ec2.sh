8c4041408f7c2c95647be8840164ef5b4263c0b0
Hoyoung Jung <honux77@gmail.com>
2015-11-18 05:23:31 +0900

    crontab and log script

--- aws/cli/ec2.sh
+++ aws/cli/ec2.sh
@@ -10,3 +10,14 @@ aws ec2 run-instances --image-id $LINUX_AMI --count 1 --instance-type t2.micro -
 
 # find instance by tag name
 aws ec2 describe-instances --filter 'Name=tag:Name,Values=watple' --query Reservations[0].Instances[0].InstanceId
+
+
+# create snapshot for every 6 hour
+echo "* */6 * * * aws ec2 create-snapshot --volume-id <volume-id> 2>&1 >> /tmp/cronlog" > cronjob
+crontab cronjob
+
+# cron reset
+crontab -r
+
+# generate log file name
+echo "I am some file" > "hoyoung-$(date +'%m-%d-%Y-%H:%M:%S').log"
