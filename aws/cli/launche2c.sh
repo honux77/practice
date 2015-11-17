34b80bc68ce0aeaadf19d69e2b08451af5d8c52a
Hoyoung Jung <honux77@gmail.com>
2015-11-17 17:55:35 +0900

    aws ec2 launching script

--- aws/cli/launche2c.sh
+++ aws/cli/launche2c.sh
@@ -1,2 +1,5 @@
-aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t1.micro --key-name MyKeyPair \
+#!/bin/bash
+# launch ec2 instances
+# Amazon linux ap-northeast-1 : ami-383c1956
+aws ec2 run-instances --image-id ami-383c1956 --count 1 --instance-type t2.micro --key-name MyKeyPair \
 --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
