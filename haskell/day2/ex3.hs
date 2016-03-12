2100b902f53d3059c54ac2029f3a76746428ab5e
honux77 <honux77@gmail.com>
2016-03-12 06:47:47 +0000

    study2 ex3 update

--- haskell/day2/ex3.hs
+++ haskell/day2/ex3.hs
@@ -1,3 +1,3 @@
 swap tpls = [ (y,x) | (x,y) <- tpls ]
-sum2 nx = (head nx) + (if tail nx /= [] then sum2 (tail nx) else 0)
+sum2 nx = if null nx then 0 else head nx + sum2 (tail nx)
 divisors m = [ x | x <- [1..m], m `mod` x == 0 ]
