69db97969265b14cb0c48fa434ce98abff6c4608
Hoyoung Jung <k502000@naver.com>
2016-06-02 01:31:19 +0000

    learn ruby hard way ex1

---
+++ ruby/learn_ruby_hard_way/ex1.rb
@@ -0,0 +1,8 @@
+# ex1.rb
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
