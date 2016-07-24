81bb9eb2fa9ba0323430abca6b8f0591247cc059
Hoyoung Jung <honux77@gmail.com>
2016-07-24 22:04:08 +0900

    java book (GoJ) day2

---
+++ java/java-book/MyObject.java
@@ -0,0 +1,8 @@
+public class MyObject {
+	public static void main(String[] args) {
+		MyObject[] array = new MyObject[1];
+		array[0] = new MyObject();
+		System.out.println(array);
+		System.out.println(array[0]);
+	}
+}
