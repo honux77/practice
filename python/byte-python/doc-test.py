6369d750f091dce98bde2c784d272ddf6006fa29
Hoyoung Jung <honux77@gmail.com>
2017-03-21 10:02:40 +0900

    Example of help function and docString

---
+++ python/byte-python/doc-test.py
@@ -0,0 +1,13 @@
+def printString(text, num = 1):
+    '''Print text multiple times. 
+    text - text to print
+    num - number of repeat'''
+    for _ in range(num):
+        print(text)
+
+
+help(printString)
+
+printString("hello, world");
+
+printString("hello, world");
