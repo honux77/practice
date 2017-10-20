d76312e7e96ec37fabeaf692fde2674a7ab5ddac
Hoyoung Jung <honux77@gmail.com>
2017-10-21 02:37:27 +0900

    Implement JS stdin example

---
+++ node/example/stdin2.js
@@ -0,0 +1,28 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    console.log(arr);
+
+}
