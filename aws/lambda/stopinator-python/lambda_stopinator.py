0e845127163b00e591929c319ff79d4dc03d4a38
Jingoo <honux77@gmail.com>
2017-11-12 19:41:39 +0900

    Add aws lambda python stopinator

---
+++ aws/lambda/stopinator-python/lambda_stopinator.py
@@ -0,0 +1,36 @@
+import boto3
+regionList=['ap-northeast-2']
+
+def findTag(instance, key, value):
+    tags = instance.tags
+    target_tag = ""
+    if tags is None:
+        print("No tags")
+        return True
+    # find tag value of key
+    for tag in tags:
+        if tag['Key'] == key: 
+            target_tag = tag['Value']
+            break
+    if target_tag in value:
+        print("{}: {}".format(key, target_tag))
+        return True
+    return False
+
+def stopInstances(tag, value, region):
+    targets = []
+    print("region:", region)
+    ec2 = boto3.resource('ec2', region)
+    instances = ec2.instances.all()
+    for instance in instances:
+        if findTag(instance, tag, value): 
+            targets.append(instance)
+
+    if len(targets) == 0:
+        print("Target instance not found.")
+    else:
+        for instance in targets:
+            print("stop {}".format(instance.instance_id))
+            instance.stop()
+
+stopInstances('Environment', 'dev', 'ap-northeast-2')
