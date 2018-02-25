91b99d08892e8fc758c21a2ddec13ba6f83f4f8b
Hoyoung Jung <honux77@gmail.com>
2018-02-25 23:44:38 +0900

    [algorithm] Solve hackerrank linked list problem
    This is the solution of "get-the-value-of-the-node-at-a-specific-position-from-the-tail" problem.

---
+++ hackerrank/linked-list/position-from-tail.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/forum
+
+def GetNode(head, position):
+    curr = head
+    tail = head
+    for i in range(position):
+        tail = tail.next
+    while tail.next is not None:
+        curr = curr.next
+        tail = tail.next
+    return curr.data
