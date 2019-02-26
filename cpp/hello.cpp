4adf25f06d345b2dfaf744693149fd2aeb96c50b
Jingoo <honux77@gmail.com>
2019-02-26 16:12:07 +0900

    Add cpp hello world template

---
+++ cpp/hello.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using i64 = long long int;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+    cout << n;
+    return 0;
+}
