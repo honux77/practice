3d1b0a9afa95c7acbe7c3ac05a06c4adda845e34
Jingoo <honux77@gmail.com>
2019-06-13 17:47:27 +0900

    Study promise
    참고링크의 아래 부분까지 읽었음
    
    앞에서 프로미스 에러 처리 방법 2가지를 살펴봤습니다. 개개인의 코딩 스타일에 따라서 then()의 두 번째 인자로 처리할 수도 있고 catch()로 처리할 수도 있겠지만 가급적 catch()로 에러를 처리하는 게 더 효율적입니다.

--- javascript/promise/test2.js
+++ javascript/promise/test2.js
@@ -2,8 +2,16 @@ const p = new Promise((resolve, reject) => {
 	resolve(100);
 });
 
+const q = new Promise((resolve, reject) => {
+	reject(200);
+});
+
 p.then((data) => {
 	console.log(data);
+});
+
+q.catch((err) =>  {
+	console.log(err);
 });
 
 console.log("End of code");
