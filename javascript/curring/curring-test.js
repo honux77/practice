8f51204433101eb6a31578f002262204a9bb301e
Jingoo <honux77@gmail.com>
2019-07-08 15:10:12 +0900

    es6 partial application

--- javascript/curring/curring-test.js
+++ javascript/curring/curring-test.js
@@ -2,9 +2,12 @@ const div = (n, m) => (n/m);
 const mdiv = n => m => div(n, m);
 const udiv = (n, m) => mdiv(n)(m);
 
+const tdiv = m => div(10, m);
+
 //all the same
 console.log(div(10,3));
 console.log(mdiv(10)(3));
 console.log(udiv(10,3));
+console.log(tdiv(3));
 
 
