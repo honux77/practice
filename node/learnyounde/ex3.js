8fe976ff1407cdb7ed482b6c3e622aafc3947359
Jingoo <honux77@gmail.com>
2019-07-15 17:56:09 +0900

    learnyounode ex3

--- node/learnyounde/ex3.js
+++ node/learnyounde/ex3.js
@@ -1,5 +1,3 @@
-var fs = require('fs');
-var buffer = fs.readFileSync(process.argv[2]);
-var text = buffer.toString();
-var lines = text.split("\n");
-console.log(lines.length - 1);
+const fs = require('fs');
+const str = fs.readFileSync(process.argv[2]).toString();
+console.log(str.split("\n").length - 1);
