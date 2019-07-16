1b37850f9abced2c5df66911a67b6fb9dadf3d0d
Jingoo <honux77@gmail.com>
2019-07-16 14:39:52 +0900

    [nodejs] Solve learnyounode ex4 again

--- node/learnyounde/ex4.js
+++ node/learnyounde/ex4.js
@@ -1,8 +1,4 @@
-var fs = require('fs');
-fs.readFile(process.argv[2], function(err, data) {
-	if(err) throw err;
-	var text = data.toString();
-	var lines = text.split("\n");
-	console.log(lines.length - 1);
-});
-
+var fs = require('fs')
+fs.readFile(process.argv[2], (err, data) => {
+	console.log(data.toString().split("\n").length - 1)
+})
