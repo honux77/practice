69045416b601cb3f0cf41f4fd816f7b513b3cf84
Jingoo <honux77@gmail.com>
2019-09-27 14:31:20 +0900

    Update docker mysql root passowrd
    매번 입력하기 귀찮아서 루트 패스워드를 간단하게 업데이트했다.

--- docker/docker-mysql.sh
+++ docker/docker-mysql.sh
@@ -1,1 +1,1 @@
-docker run --name=honux-mysql -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=9a55w0rd -p 3306:3306 -d mysql/mysql-server:5.7
+docker run --name=honux-mysql -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=dbman1004 -p 3306:3306 -d mysql/mysql-server:5.7
---
+++ node/worker/worker-test.js
@@ -0,0 +1,23 @@
+const {
+  Worker, isMainThread, parentPort, workerData
+} = require('worker_threads');
+
+if (isMainThread) {
+  module.exports = async function parseJSAsync(script) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(__filename, {
+        workerData: script
+      });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    });
+  };
+} else {
+  const { parse } = require('some-js-parsing-library');
+  const script = workerData;
+  parentPort.postMessage(parse(script));
+}
