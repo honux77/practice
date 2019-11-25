38e8d634fbb3bfe96e58c4ff85baaaeb98d1487b
Jingoo <honux77@gmail.com>
2019-11-25 14:59:33 +0900

    [node, sequelize] Learn Sequelize
    Seqeulize quick start guide 실행시켜 봄
    공식 홈페이지 참고

---
+++ node/learn-sequelize/quick-start.js
@@ -0,0 +1,17 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+class User extends Model {}
+User.init({
+  username: DataTypes.STRING,
+  birthday: DataTypes.DATE
+}, { sequelize, modelName: 'user' });
+
+sequelize.sync()
+  .then(() => User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
