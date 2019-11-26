6b340f5f3a10a1e8e6b0211d7627be394b183518
Jingoo <honux77@gmail.com>
2019-11-26 15:25:42 +0900

    [js] study js hoisting
    JS Hoisting 공부함

---
+++ javascript/hoisting/study.md
@@ -0,0 +1,3 @@
+# Hoisting
+
+https://developer.mozilla.org/ko/docs/Glossary/Hoisting
---
+++ javascript/hoisting/test-hoisting.js
@@ -0,0 +1,10 @@
+var x = 1; // x 초기화
+console.log(x + " " + y); // '1 undefined'
+var y = 2;
+
+
+// 아래 코드는 이전 코드와 같은 방식으로 동작합니다.
+var x = 1; // Initialize x
+var y; // Declare y
+console.log(x + " " + y); // '1 undefined'
+y = 2; // Initialize y
