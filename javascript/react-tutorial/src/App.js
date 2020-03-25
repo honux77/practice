c14a1f0dc332120b2b2e69e49f56964cfa5779e5
Hoyoung Jung <honux77@gmail.com>
2020-03-25 12:03:06 +0900

    Small change and test ok

--- javascript/react-tutorial/src/App.js
+++ javascript/react-tutorial/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React!
         </p>
         <a
           className="App-link"
