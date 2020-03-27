633df73714dfaf3184cbdda81a812726e2a6dbed
Hoyoung Jung <honux77@gmail.com>
2020-03-27 10:00:57 +0900

    [react][js] Apply state
    스테이트를 사용해 봤음
    나중에 이벤트 처리에 유용하게 사용될 듯한 느낌

--- javascript/react-tutorial/src/App.js
+++ javascript/react-tutorial/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Subject from './components/Subject';
 import Contents from './components/Contents';
+import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Subject title="찢어진 바지" author="스폰지밥"></Subject>
-      <Contents></Contents>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title: "찢어진 바지",author: "스폰지밥"}
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} author={this.state.subject.author}></Subject>
+        <Contents></Contents>
+      </div>
+    );
+  }
 }
 
 export default App;
