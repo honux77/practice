625e32e59047ef27f20afe3da8f14a831abcfa15
Hoyoung Jung <honux77@gmail.com>
2020-03-27 12:18:40 +0900

    REACT, JS: Apply state and key
    재밌다!

--- javascript/react-tutorial/src/App.js
+++ javascript/react-tutorial/src/App.js
@@ -9,14 +9,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subject: {title: "찢어진 바지",author: "스폰지밥"}
+      subject: {title: "찢어진 바지",author: "스폰지밥"},
+      people: [
+        {id:1, name: '징징이'},
+        {id:2, name: '다람이'},
+        {id:3, name: '뚱이'}
+      ]
     }
   }
   render() {
     return (
       <div className="App">
         <Subject title={this.state.subject.title} author={this.state.subject.author}></Subject>
-        <Contents></Contents>
+        <Contents people={this.state.people}></Contents>
       </div>
     );
   }
