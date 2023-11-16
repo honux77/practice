86fbb21377e3514c72ca1edcb56dd22f00f74f70
Hoyoung Jung <honux77@gmail.com>
2023-11-16 16:20:20 +0900

    레스트 퀵 가이드 학습중 - 의존성 추가

--- rust/learn.md
+++ rust/learn.md
@@ -1,8 +1,12 @@
 # Learn rust
 
+## getting-started
+
+- https://www.rust-lang.org/learn/get-started
+
 ## rust tutorial
 
--  https://doc.rust-lang.org/book/ch01-02-hello-world.html
+- https://doc.rust-lang.org/book/ch01-02-hello-world.html
 
 ## wasm tutorial: game of life
 
--- rust/tutorial/hello-rust/src/main.rs
+++ rust/tutorial/hello-rust/src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
