d2acd732902fd6f6e182971a45a747ea1c910237
Hoyoung Jung <honux77@gmail.com>
2024-10-24 08:31:46 +0900

    러스트 표준 입출력 사용해 봄

--- rust/tutorial/guessing_game/src/main.rs
+++ rust/tutorial/guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
