22d90751f1b38dd86ed51c2a3ebf2b3780f69097
Hoyoung Jung <honux77@gmail.com>
2024-10-27 01:26:11 +0900

    Rust 뽑기 에러 수정

--- rust/winner/src/main.rs
+++ rust/winner/src/main.rs
@@ -1,6 +1,5 @@
-// winner.rs
-
-use std::io::{self, Write};
+use std::io;
+use rand::Rng;
 
 fn main() {
     println!("참가자 숫자를 입력해 주세요.");
@@ -9,10 +8,9 @@ fn main() {
     let participants: u32 = input.trim().parse().expect("유효한 숫자를 입력해 주세요.");
 
     let winner = generate_winner(participants);
-    println!("당첨자는 {}번째 참가자입니다.", winner);
+    println!("당첨자는 {winner}번째 참가자입니다.");
 }
 
 fn generate_winner(participants: u32) -> u32 {
-    let mut rng = thread_rng();
-    rng.gen_range(1..=participants)
+    rand::thread_rng().gen_range(1..=participants)
 }
