e42083b2646bff7cf3d870b40ba4af995e67f5fb
Hoyoung Jung <honux77@gmail.com>
2025-09-18 19:13:23 +0900

    타입스크립트 - type과 클래스

---
+++ typescript/type.ts
@@ -0,0 +1,17 @@
+interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("Murphy", 1);
+console.log(user);
